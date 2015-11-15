71ac14ec6717b90b6cc211f4c2812c0650758dc4
guobao.jiang <loveaborn@foxmail.com>
2015-11-15 22:42:25 +0800

    fix page onfinished load bug

--- assets/range.js
+++ assets/range.js
@@ -1,5 +1,10 @@
+$(function(){
+    $('input[type="range"]').each(eachInputRangeAction)
+})
 
-$('input[type="range"]').on('input', function () {
+$('input[type="range"]').on('input', eachInputRangeAction)
+
+function eachInputRangeAction() {
     var percent = Math.ceil(((this.value - this.min) / (this.max - this.min)) * 100);
 
     $(this).parent().find('.detail').html(this.value + '%')
@@ -9,4 +14,4 @@ $('input[type="range"]').on('input', function () {
     } else {
         $(this).css('background', '-webkit-linear-gradient(left, #65999c 0%, red ' + percent + '%, #ddd ' + percent + '%)');
     }
-})
+}
