7b65f9b1f33bd43d243664aae93c4e154f30ec7b
蒋国宝 <loveaborn@foxmail.com>
2015-11-17 23:53:42 +0800

    source code extend jQuery

--- assets/range.js
+++ assets/range.js
@@ -1,58 +1,96 @@
-$(function(){
+/*$(function(){
     $('input[type="range"]').each(eachInputRangeAction)
-})
+})*/
 
-$('input[type="range"]').on('input', eachInputRangeAction)
+//$('input[type="range"]').on('input', eachInputRangeAction)
 
-/**
- * detect which kind of browser use.
- * more http://stackoverflow.com/questions/2400935/browser-detection-in-javascript
- * MSIE 8 MSIE 9 MSIE 10 IE 11
- * Chrome 41 Chrome 42 (using 42 Beta) Chrome 43 (using 43 Dev)
- * Firefox 35 Firefox 37 (using 37 Beta) Firefox 3 (usng 3.6)
- * Safari 8 Safari 6 (using 6.1) Safari 8 (using iPhone 6)
- * Chrome 4 (Samsung S5 native browser)
- * Opera 27
- */
-navigator.browserInfo= (function(){
-    var ua= navigator.userAgent, tem,
-        M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-    if(/trident/i.test(M[1])){
-        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
-        return 'IE '+(tem[1] || '');
+$ = jQuery;
+
+var config = {};
+
+var BrowserKit = (function(){
+    var defaultOptions = {
+        style : 'default',    // style 'default', 'basic'
+        color : 'red'          // '#65999c'
     }
-    if(M[1]=== 'Chrome'){
-        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
-        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
-    }
-    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
-    return { 'browser': M[0], 'version': M[1] };
-    //return M.join(' ');
-})();
 
-var config = {
-    style : 'default',    // style 'default', 'basic'
-    color : 'red'          // '#65999c'
-}
-
-
-function eachInputRangeAction() {
-    var percent = Math.ceil(((this.value - this.min) / (this.max - this.min)) * 100);
-
-    $(this).parent().find('.detail').html(this.value + '%')
-
-    // gradient depend on different browser.
-    var gradient = 'Firefox' == navigator.browserInfo.browser ? '-moz-linear-gradient' : '-webkit-linear-gradient';
-
-    if (percent == 100) {
-        $(this).css('background', gradient + '(left, #65999c 100%, red ' + percent + '%, #ddd ' + percent + '%)');
-    } else {
-        //$(this).css('background', '-webkit-linear-gradient(left, #65999c 0%, red ' + percent + '%, #ddd ' + percent + '%)');
-        if (config.style == 'basic') {
-            $(this).css('background', gradient + '(left, ' + config.color + ' 0%, '+ config.color +' ' + percent + '%, #ddd ' + percent + '%)');
+    var BrowserKit = {};
+    BrowserKit.config = function(options) {
+        if (options == undefined || options == null) {
+            config = defaultOptions;
         } else {
-            $(this).css('background', gradient + '(left, #65999c 0%, '+ config.color +' ' + percent + '%, #ddd ' + percent + '%)');
+            config = options;
         }
     }
-}
+
+    /**
+     * http://stackoverflow.com/questions/2400935/browser-detection-in-javascript
+     * @type {{browser, version}}
+     */
+    BrowserKit.browserInfo =  (function(){
+        var ua= navigator.userAgent, tem,
+            M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+        if(/trident/i.test(M[1])){
+            tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+            return 'IE '+(tem[1] || '');
+        }
+        if(M[1]=== 'Chrome'){
+            tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+            if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+        }
+        M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+        if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+        return { 'browser': M[0], 'version': M[1] };
+        //return M.join(' ');
+    })();
+
+    BrowserKit.isSupported = function() {
+        return $.inArray(BrowserKit.browserInfo.browser, ['Chrome', 'Firefox', 'Safari', 'Opera']) >= 0;
+    }
+
+    BrowserKit.eachInputRangeAction = function() {
+        var percent = Math.ceil(((this.value - this.min) / (this.max - this.min)) * 100);
+
+        $(this).parent().find('.detail').html(this.value + '%')
+
+        // gradient depend on different browser.
+        var gradient = 'Firefox' == BrowserKit.browserInfo.browser ? '-moz-linear-gradient' : '-webkit-linear-gradient';
+        /**
+         *  background color for webkit Kernel
+         *  $(this).css('background', '-webkit-linear-gradient(left, #65999c 0%, red ' + percent + '%, #ddd ' + percent + '%)');
+         */
+
+        console.log('config.color=' + config.color + ' config.style=' + config.style);
+        if (percent == 100) {
+            if (config.style == 'basic') {
+                $(this).css('background', gradient + '(left, '+ config.color +' 100%, ' + config.color +' ' + percent + '%, ' + config.color + ' ' + percent + '%)');
+            } else {
+                $(this).css('background', gradient + '(left, #65999c 100%, red ' + percent + '%, #ddd ' + percent + '%)');
+            }
+        } else {
+            if (config.style == 'basic') {
+                $(this).css('background', gradient + '(left, ' + config.color + ' 0%, '+ config.color +' ' + percent + '%, #ddd ' + percent + '%)');
+            } else {
+                $(this).css('background', gradient + '(left, #65999c 0%, '+ config.color +' ' + percent + '%, #ddd ' + percent + '%)');
+            }
+        }
+    }
+
+    return BrowserKit;
+})()
+
+$.fn.extend({
+    range: function(options) {
+        console.log('options:' + JSON.stringify(options));
+
+        if (!BrowserKit.isSupported()) {
+            return;
+        }
+
+        BrowserKit.config(options);
+
+        this.each(BrowserKit.eachInputRangeAction);
+
+        this.on('input', BrowserKit.eachInputRangeAction);
+    }
+});
